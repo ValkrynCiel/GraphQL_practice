49b9131ed8a76347f1aeec0eef4bdda71d9804e9
Ethan Shen <ethanshen.wu@gmail.com>
2019-06-11 16:41:26 -0700

    server.js: added type relation between books and authors

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -11,9 +11,9 @@ const {
 
 //dummy data
 let books = [
-  { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
-  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
-  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' }
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' }
 ]
 
 let authors = [
@@ -23,14 +23,22 @@ let authors = [
 ]
 
 /**
- * schema for BookType with id, name, genre fields
+ * schema for BookType with id, name, genre, author fields
+ * 
+ * resolve function in author uses parent param (e.g. the book that is found)
  */
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args){
+        return _.find(authors, { id: parent.authorId });
+      }
+    }
   })
 });
 
